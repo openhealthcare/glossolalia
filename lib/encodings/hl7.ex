064c4c23604105c5d4f396fab37c19dacdf5bd49
Ross Jones <ross@servercode.co.uk>
2015-05-13 13:27:35 +0100

    Working test for HL7 extraction

--- lib/encodings/hl7.ex
+++ lib/encodings/hl7.ex
@@ -1,4 +1,7 @@
 defmodule Glossolalia.Encodings.HL7 do
+  require Record
+
+  Record.defrecord :personal_info, Record.extract(:pid, from: "deps/ehl7/include/ehl7_segment.hrl")
 
   @doc """
   Takes HL7 data && decodes it.
@@ -13,5 +16,5 @@ defmodule Glossolalia.Encodings.HL7 do
   def encode(data) do
     :erlang.ehl7.encode data
   end
-  
+
 end
--- test/glossolalia_test.exs
+++ test/glossolalia_test.exs
@@ -1,7 +1,18 @@
 defmodule GlossolaliaTest do
   use ExUnit.Case
+  alias  Glossolalia.Encodings.HL7
+  require  Glossolalia.Encodings.HL7
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  @adt "PID|||PATID1234^5^M11||JONES^WILLIAM^A^III||19610615|M||C|1200 N ELM STREET^^GREENSBORO^NC^27401-1020|GL|(919)379-1212|(919)271-3434||S||PATID12345001^2^M10|123456789|987654^NC|\r"
+
+  test "extract pid string" do
+    msg = :ehl7.decode @adt
+    name_etc = :ehl7.segment  :pid, msg
+
+    assert HL7.personal_info(name_etc, :first_name) == "WILLIAM"
+    assert HL7.personal_info(name_etc, :last_name)  == "JONES"
+    assert HL7.personal_info(name_etc, :patient_id)  == "PATID1234"
   end
+
+
 end
