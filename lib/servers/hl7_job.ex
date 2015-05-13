d3fadd71dee2e577410fd89d71237a1bcdf75290
Ross Jones <ross@servercode.co.uk>
2015-05-13 17:35:57 +0100

    Accepting (at least the first) MSH and responding with an ack

--- lib/servers/hl7_job.ex
+++ lib/servers/hl7_job.ex
@@ -1,17 +1,31 @@
 defmodule Glossolalia.Servers.HL7Job do
+  require Logger
+  alias  Glossolalia.Encodings.HL7
+  require  Glossolalia.Encodings.HL7
 
   def handle_request(sender, request) do
-    IO.inspect request
-    lines = String.split(request, "\r")
-    IO.inspect lines
+    lines = String.split(request, "\n")
+    header_line = List.first(lines)
+    msg = :ehl7.decode header_line
 
-    # Store the data, process the MSH message
-    # to find the ID, return an MSH/ACK with AA.
-    response = "OK"
-
-    send sender,{:ok, response}
+    try do
+      res = :ehl7.segment(:msh, msg)
+      process_msh(sender, res, header_line)
+    rescue
+      # Process an error. Ideally have the server responder
+      # process timeout and send an AE instead.
+      e ->
+        send sender,{:ok, "Sending AE message"}
+        Logger.error(e)
+    end
   end
 
+  def process_msh(sender, msg, header_line) do
+    msg_id = HL7.message_header(msg, :message_control_id)
 
+    response = "#{header_line}\nMSA|AA|#{msg_id}\r"
+    Logger.info(response)
+    send sender,{:ok, response}
+  end
 
 end
