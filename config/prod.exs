b5bbcb61bc63c67bcdbc27780ccd92db6d511eae
fredkingham <fredkingham@gmail.com>
2016-02-02 13:46:25 +0000

    add in etc/nginx.conf

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :glossolalia, Glossolalia.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 5000],
+  url: [host: "ec2-52-16-175-249.eu-west-1.compute.amazonaws.com", port: 5000],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
